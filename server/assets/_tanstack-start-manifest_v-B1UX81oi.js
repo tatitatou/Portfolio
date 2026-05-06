06e2c582c163ecc6a033e321691f8f8f2db655ef
tatitatou <tatiana.novion7@gmail.com>
2026-05-06 23:32:36 +0200

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-B1UX81oi.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Portfolio/browse-and-bloom-65/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/Portfolio/assets/index-ujz20amt.js"] }, "/": { filePath: "C:/Portfolio/browse-and-bloom-65/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/Portfolio/assets/index-CQDM4ur9.js"] } }, clientEntry: "/Portfolio/assets/index-ujz20amt.js" });
+export {
+  tsrStartManifest
+};
